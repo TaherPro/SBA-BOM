b426ebd1483068c42fa2e8dba8742cb0f400da8d
TaherPro <alfakihtaher@gmail.com>
2025-09-18 20:22:16 -0400

    commit

--- app.js
+++ app.js
@@ -32,3 +32,38 @@ li.textContent = "Subject - Description - Due Date - [Type]";
 fragment.appendChild(li);
 submissionList.appendChild(fragment);
 
+// submission form validation
+submissionForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (!selectedSubject) {
+        alert("please select a subject first!");
+        return;
+    }
+    if (titleInput.value.length < 5) {
+        alert("Title must be at least 5 characters!");
+        return;
+    }
+    if (!dateInput.value) {
+        alert("Please select date!");
+        return;
+    }
+    let selectedType = null;
+    radioInputs.forEach((radio) => {
+        if (radio.checked) selectedType = radio.value;
+    });
+    if (!selectedType) {
+        alert("Please select submission type!");
+        return;
+    }
+    // create submission item 
+    const li = document.createElement("li");
+    li.textContent = `${selectedSubject} - ${titleInput.value} - ${dateInput.value} - [${selectedType.toUpperCase()}]`;
+    li.setAttribute("data-subject", selectedSubject);
+    li.setAttribute("data-type", selectedType);
+    submissionList.appendChild(li);
+
+    submissionCount++;
+    totalCountElem.textContent = `Total Submissions: ${submissionCount}`;
+
+});
